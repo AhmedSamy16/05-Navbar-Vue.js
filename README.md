b9163e1df07b9bd036c49fcb9bc8fadaf07f69f6
AhmedSamy16 <ahmed.samy1640@gmail.com>
2024-03-04 12:40:18 +0200

    Project Completed

---
+++ README.md
@@ -0,0 +1,6 @@
+# 05-Navbar-vue
+
+This is project 5/50 of my Vue.js projects series. 
+it's a fully responsive navbar on Vue.js using CSS grid and flexbox. Additionally, display breadcrumbs with the menu dropdown on mobile screen size.
+
+[Live Demo Click Here](https://05-navbar-reactjs.netlify.app/)
---
+++ src/components/Navbar.vue
@@ -0,0 +1,37 @@
+
+<template>
+    <nav>
+        <div class="nav-logo">
+            <h1>Vue.js</h1>
+            <img class="react-icon" src="/images/vue.png" alt="React">
+        </div>
+        <div class="menu-icon" @click="handleClick">
+            <img v-if="data.isClicked" src="/images/close.png" alt="close icon">
+            <img v-else src="/images/menu.png" alt="menu icon">
+        </div>
+        <ul class="nav-menu" :class="data.isClicked ? 'active' : ''">
+            <li v-for="item in data.items" :key="item.title">
+                <a :class="item.cName" :href="item.url">{{ item.title }}</a>
+            </li>
+        </ul>
+        <Button>
+            Sign in
+        </Button>
+    </nav>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import MenuItems from '@/utils/menuItems';
+import Button from './Button.vue';
+
+const data = ref({
+    isClicked: false,
+    items: MenuItems
+})
+
+const handleClick = () => {
+    data.value.isClicked = !data.value.isClicked
+}
+
+</script>
